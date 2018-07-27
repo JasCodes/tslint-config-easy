c461b95050b13c8b3726c149257dcc5efd30aba0
Jaspreet Singh <dev@jas.bio>
2018-07-27 18:10:52 +0530

    Add tslint.js

---
+++ tslint.js
@@ -0,0 +1,28 @@
+module.exports = {
+    "defaultSeverity": "error",
+    "extends": [
+        "tslint:recommended"
+    ],
+    "jsRules": {},
+    "rules": {
+        "quotemark": [
+            true,
+            "single"
+        ],
+        "ordered-imports": false,
+        "import-spacing": false,
+        "space-before-function-paren": false,
+        "one-line": false,
+        "object-literal-sort-keys": false,
+        "no-console": false,
+        "no-unused-expression": false,
+        "member-access": false,
+        "trailing-comma": false,
+        "max-classes-per-file": false,
+        "arrow-parens": false,
+        "variable-name": false,
+        "interface-name": false,
+        "no-empty-interface": false
+    },
+    "rulesDirectory": []
+}
